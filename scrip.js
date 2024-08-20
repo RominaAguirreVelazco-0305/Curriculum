f7525141cdffca585d723bfb1ecf3fce6cd6e508
Romina Jacqueline Aguirre Velazco <romina.aguirre8841@alumnos.udg.mx>
2024-08-19 20:54:54 -0600

    Firstcommit

---
+++ scrip.js
@@ -0,0 +1,3 @@
+document.getElementById("contactButton").addEventListener("click", function() {
+    alert("Thank you for your interest! You can contact me at romina.aguirre8841@alumnos.udg.mx");
+});
